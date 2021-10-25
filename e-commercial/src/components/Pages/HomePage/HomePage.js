3fd523a893b24401621c9ad096f3e10380cd4385
BO WU <bwuu0013@student.monash.edu>
2021-10-25 20:08:35 +1100

    setup the homepage and menuItem

---
+++ e-commercial/src/components/Pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HomePage.scss';
+import Directory from '../../Directory';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
