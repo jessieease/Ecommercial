08357d8ebd21cbb39e5901c6636d83fa37189a35
BO WU <bwuu0013@student.monash.edu>
2021-10-26 21:32:43 +1100

    add shop view based on collection and header

--- e-commercial/src/App.js
+++ e-commercial/src/App.js
@@ -3,18 +3,15 @@ import './App.css';
 import { Route } from 'react-router-dom';
 
 import HomePage from './components/Pages/HomePage';
-
-const HatPage = () => (
-  <div>
-    <h1>HAT PAGE</h1>
-  </div>
-);
+import ShopPage from './components/Pages/ShopPage';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="body">
+      <Header />
       <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={HatPage} />
+      <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
