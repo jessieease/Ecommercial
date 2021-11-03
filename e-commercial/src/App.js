210cd61b31735d712d912096c064e22384c5cd38
BO WU <bwuu0013@student.monash.edu>
2021-11-03 18:23:55 +1100

    add redirect to home if user sign in

--- e-commercial/src/App.js
+++ e-commercial/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
 
 import HomePage from './components/Pages/HomePage';
 import ShopPage from './components/Pages/ShopPage';
@@ -43,13 +43,27 @@ class App extends React.Component {
         <Header />
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/sign-in' component={SignInPage} />
+        <Route
+          exact
+          path='/sign-in'
+          render={() => 
+            this.props.currentUser ? (
+              <Redirect to='/' />
+              ) : (
+              <SignInPage />
+              )
+          }
+        />
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
